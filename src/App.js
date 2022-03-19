0a8b3856f2932b8d678f0ddf6dbe3220fa9e734a
sadik126 <sadikhimel04@gmail.com>
2022-03-19 20:01:05 +0600

    m

--- src/App.js
+++ src/App.js
@@ -1,20 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState , useEffect} from 'react';
+
 
 function App() {
   return (
     <div className="App">
+      
+      <Loadcountries></Loadcountries>
       
     </div>
   );
 }
 
 
-function loadcountries(){
-  return 
-  <div>
+function Loadcountries(){
+  const [countries,setCountries] = useState([])
 
+ useEffect(()=>{
+  fetch('https://restcountries.com/v3.1/all')
+  .then(res => res.json())
+  .then(data => setCountries(data))
+ },[])
+
+  return <div>
+    <h1>visiting country of the world!!!</h1>
+    <h3>available countries: {countries.length}</h3>
+    {
+      countries.map(country => <Country name={country.name.common} population={country.population}></Country>)
+    }
+  </div>
+ 
+}
+
+function Country(props){
+  return <div>
+    <h2>Name:{props.name}</h2>
+    <h4>Population:{props.population}</h4>
   </div>
 }
+
+
 
 export default App;
