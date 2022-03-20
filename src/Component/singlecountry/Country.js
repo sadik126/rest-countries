8d7221f6584830a1ae552c204406273feaeeabb3
sadik126 <sadikhimel04@gmail.com>
2022-03-20 19:43:12 +0600

    m

--- src/Component/singlecountry/Country.js
+++ src/Component/singlecountry/Country.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import './Country.css'
 
-const Country = () => {
+const Country = (props) => {
     return (
-        <div>
-            <h2>Country name:</h2>
+        <div className='country'>
+            <h2>Country name:{props.name}</h2>
+            <h3>population: {props.population}</h3>
         </div>
     );
 };
